0f4dcfcd0b22a0361f84b4887822b1d59fa68921
John Xander <johnblox5@gmail.com>
2023-02-11 21:02:52 +0000

    add NotesList and UsersList componentds

--- src/App.js
+++ src/App.js
@@ -4,16 +4,31 @@ import Public from './components/Public'
 import Login from './features/auth/Login'
 import DashLayout from './components/DashLayout'
 import Welcome from './features/auth/Welcome'
+import NotesList from './features/notes/NotesList'
+import UsersList from './features/users/UsersList'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
-      <Route index element={<Public />} />
-      <Route path='login' element={<Login />} />
+      <Route path="/" element={<Layout />}>
 
-      <Route path='dash' element={<DashLayout />}>
-        <Route index element={<Welcome />} />
+        <Route index element={<Public />} />
+        <Route path='login' element={<Login />} />
+
+        <Route path='dash' element={<DashLayout />}>
+
+          <Route index element={<Welcome />} />
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+
+        </Route>{/* End Dash */}
+        
       </Route>
 
     </Routes>
